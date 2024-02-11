fe778d5a9f5526d1956d2e43617349e1c262ac43
alynayoung <158622599+alynayoung@users.noreply.github.com>
2024-02-12 07:19:34 +0900

    Time: 2213 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,14 +1,11 @@
-WITH first_play AS (
-    SELECT player_id, MIN(event_date) AS first_day
+WITH first_activity AS (
+    SELECT player_id
+         , MIN(event_date) AS first_event_date
     FROM Activity
     GROUP BY player_id
 )
-, next_day_play AS (
-    SELECT a.player_id
-    FROM Activity a
-    JOIN first_play fp ON a.player_id = fp.player_id
-    WHERE a.event_date = DATE_ADD(fp.first_day, INTERVAL 1 DAY)
-)
-SELECT ROUND(COUNT(next_day_play.player_id) / COUNT(first_play.player_id), 2) AS fraction
-FROM first_play
-LEFT JOIN next_day_play ON first_play.player_id = next_day_play.player_id;
+
+SELECT ROUND(COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM Activity a
+INNER JOIN first_activity f ON a.player_id = f.player_id
+WHERE a.event_date = DATE_ADD(f.first_event_date, INTERVAL 1 DAY)
