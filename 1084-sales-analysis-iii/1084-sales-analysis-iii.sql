7ca857f57eb1c4c1af70dfb6293f7f580b039134
alynayoung <158622599+alynayoung@users.noreply.github.com>
2024-02-12 13:57:09 +0900

    Time: 1861 ms (60.04%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,7 +1,6 @@
-select distinct
-p.product_id,
-p.product_name
-from Product p
-left join Sales s on p.product_id = s.product_id
-where sale_date between '2019-01-01' and '2019-03-31'
-and p.product_id not in (select distinct product_id from Sales where sale_date > '2019-03-31' or sale_date < '2019-01-01')
+SELECT s.product_id
+     , p.product_name
+FROM Sales AS s
+        LEFT JOIN Product AS p ON s.product_id = p.product_id
+GROUP BY s.product_id
+HAVING min(s.sale_date) >= '2019-01-01' AND max(s.sale_date) <= '2019-03-31'
