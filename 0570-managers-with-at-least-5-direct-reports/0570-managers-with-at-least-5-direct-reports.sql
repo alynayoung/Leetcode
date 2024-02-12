129bb3cdfb83ca5e34f567169d2d84e82dba7350
alynayoung <158622599+alynayoung@users.noreply.github.com>
2024-02-12 10:33:47 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,11 @@
-SELECT name 
-FROM Employee 
-WHERE id IN (
-    SELECT managerId 
-    FROM Employee 
-    GROUP BY managerId 
-    HAVING COUNT(*) >= 5)
+WITH m AS (
+    SELECT m.name
+        , COUNT(e.id) AS cnt
+    FROM Employee AS e
+        LEFT JOIN Employee AS m ON e.managerID = m.Id
+    GROUP BY m.name
+)
+
+SELECT name
+FROM m
+WHERE cnt >= 5
