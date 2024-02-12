9a12a942e8c32e081f72f44da39f75acce17b763
alynayoung <158622599+alynayoung@users.noreply.github.com>
2024-02-12 12:46:31 +0900

    Time: 507 ms (71.19%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,15 +1,18 @@
-With CTE As(
-    SELECT requester_id
-        , accepter_id
+WITH cte AS(
+    SELECT requester_id AS user_id
+         , COUNT(requester_id) AS count1
     FROM RequestAccepted
-    UNION ALL
-    SELECT accepter_id , requester_id
+    GROUP BY requester_id
+UNION ALL
+    SELECT accepter_id AS user_id
+         , COUNT(accepter_id) AS count1
     FROM RequestAccepted
+    GROUP BY accepter_id
     )
 
-select requester_id as id
-     , count(requester_id) as num
-from CTE
-group by requester_id
-order by num desc
-limit 1
+SELECT user_id AS id
+     , sum(count1) AS num
+FROM cte
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
